488555b79e62a74c88a040d3797393198466aa03
Shreedhar Acharya <spacharya2537@gmail.com>
2024-04-17 22:16:25 -0400

    Header Component

---
+++ linkit/src/Components/Core/Header/Header.jsx
@@ -0,0 +1,29 @@
+/*
+-----------------------------------------------------------
+Group Name   : Group 4
+Project Name : Linkit
+Student Name : Shree Dhar Acharya 
+Student Id   : 8899288
+Github User  : AcharyaShreedhar
+Date         : 11th April 2024
+------------------------------------------------------------
+*/
+import React from 'react';
+
+function Header() {
+  return (
+    <head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>LinkIt</title>
+      <link rel="icon" href="images/logo.png" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link href="https://fonts.googleapis.com/css2?family=Michroma&display=swap" rel="stylesheet" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      <link rel="stylesheet" href="style.css" />
+    </head>
+  );
+}
+
+export default Header;
